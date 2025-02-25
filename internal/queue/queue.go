24f9a4eadf8065efe3b833a24c5f69eeb32d52b6
Taylan Sahin <info@taylansahin.net>
2025-02-25 15:54:07 +0000

    adding basic file logging to the queue

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,14 +1,44 @@
 package queue
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
 
 type Queue struct {
-	messages []string
-	mu       sync.RWMutex
+	messages      []string
+	mu            sync.RWMutex
+	logFile       *os.File
+	enableLogging bool
 }
 
 func NewQueue() *Queue {
-	return &Queue{messages: []string{}}
+	return &Queue{messages: []string{}, enableLogging: false}
+}
+
+func (q *Queue) EnableLogging(logPath string) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	dir := logPath[:len(logPath)-len("/queue.log")]
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failure in creating log directory: %w", err)
+	}
+
+	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failure in opening log file: %w", err)
+	}
+
+	q.logFile = file
+	q.enableLogging = true
+
+	for _, msg := range q.messages {
+		fmt.Fprintf(q.logFile, "ENQUEUE %s %s\n", time.Now().Format(time.RFC3339), msg)
+	}
+	return nil
 }
 
 func (q *Queue) Enqueue(msg string) {
@@ -32,4 +62,14 @@ func (q *Queue) Size() int {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 	return len(q.messages)
+}
+
+func (q *Queue) Close() error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if q.logFile != nil {
+		return q.logFile.Close()
+	}
+	return nil
 }
